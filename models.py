0d009ca4ef805c479d248b9337a85bd31483be9f
aquashdw <aquashdw@gmail.com>
2024-06-06 16:49:49 +0900

    create more models

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
-from sqlalchemy import BigInteger, Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import List
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
@@ -9,6 +16,28 @@ class Base(DeclarativeBase):
 class TestCase(Base):
     __tablename__ = "test_case"
     id: Mapped[int] = mapped_column(primary_key=True)
-    prob_id: Mapped[int] = mapped_column(BigInteger())
+    prob_id: Mapped[int] = mapped_column(ForeignKey("problem.id"))
+    problem: Mapped["Problem"] = relationship(back_populates="test_cases")
     input: Mapped[str] = mapped_column(Text())
     output: Mapped[str] = mapped_column(Text())
+
+
+class Problem(Base):
+    __tablename__ = "problem"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    prob_desc: Mapped[str] = mapped_column(Text())
+    input_desc: Mapped[str] = mapped_column(Text())
+    output_desc: Mapped[str] = mapped_column(Text())
+    timeout: Mapped[int] = mapped_column(Integer())
+    memory: Mapped[int] = mapped_column(Integer())
+    test_cases: Mapped[List["TestCase"]] = relationship(back_populates="problem")
+
+
+class Solution(Base):
+    __tablename__ = "solution"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    prob_id: Mapped[int] = mapped_column(ForeignKey("problem.id"))
+    lang: Mapped[str] = mapped_column(String(255))
+    status: Mapped[str] = mapped_column(String(255))
+    code: Mapped[str] = mapped_column(Text())
+    score: Mapped[int] = mapped_column(Integer())
--- repository.py
+++ repository.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
-from models import TestCase
+from models import TestCase, Problem, Solution
 
 engine = create_engine("postgresql://tutor:password@localhost/tutor")
 
@@ -10,4 +10,7 @@ if __name__ == '__main__':
     with SessionMaker() as session:
         statement = select(TestCase).where(TestCase.id.__eq__(1))
         print(session.scalar(statement).input)
-
+        statement = select(Problem).where(Problem.id.__eq__(1))
+        for test_case in session.scalar(statement).test_cases:
+            print(test_case.input)
+            print(test_case.output)
